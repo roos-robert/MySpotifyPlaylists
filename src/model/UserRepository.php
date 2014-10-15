515940db6ba0e307f1239421c90d821245c010ce
rr222cy <rr222cy@student.lnu.se>
2014-10-15 14:51:05 +0200

    UserRepository.php added

---
+++ src/model/UserRepository.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace model;
+
+require_once("./src/model/UserModel.php");
+require_once("./src/model/Repository.php");
+
+class UserRepository extends Repository {
+
+    private static $username = "Username";
+    private static $password = "Password";
+    private static $key = "LoginToken";
+    private static $email = "Email";
+    private static $name = "Name";
+    private $user;
+
+    public function __construct(User $user) {
+        $this->dbTable = "user";
+        $this->user = $user;
+    }
+
+    public function add($username, $password, $email, $name) {
+        $randomKey = md5(time());
+        $db = $this->connection();
+        $sql = "INSERT INTO $this->dbTable(" . self::$username . ", " . self::$password . ", " . self::$key . ", " . self::$email . ", " . self::$name . ") VALUES (?, ?, ?, ?, ?)";
+        $params = array($username, md5($password), $randomKey, $email, $name);
+        $query = $db->prepare($sql);
+        $query->execute($params);
+    }
+
+    public function get($username) {
+        $db = $this->connection();
+        $sql = "SELECT * FROM $this->dbTable WHERE " . self::$username . " = ?";
+        $params = array($username);
+        $query = $db->prepare($sql);
+        $query->execute($params);
+        $result = $query->fetch();
+        if($result)
+        {
+            $this->user->setUser($result[self::$username], $result[self::$password], $result[self::$key]);
+            return $result;
+        }
+        return NULL;
+    }
+
+    public function getKey($key) {
+        $db = $this->connection();
+        $sql = "SELECT * FROM $this->dbTable WHERE " . self::$key . " = ?";
+        $params = array($key);
+        $query = $db->prepare($sql);
+        $query->execute($params);
+        $result = $query->fetch();
+        if($result)
+        {
+            $this->user->setUser($result[self::$username], $result[self::$password], $result[self::$key]);
+            return $result;
+        }
+        return NULL;
+    }
+}
