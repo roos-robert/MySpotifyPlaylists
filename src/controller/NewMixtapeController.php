4ab2c2c7c5186581f49329f7cf66cc06bb5a512f
rr222cy <rr222cy@student.lnu.se>
2014-10-15 16:34:06 +0200

    Working on form for NewMixtape

--- src/controller/NewMixtapeController.php
+++ src/controller/NewMixtapeController.php
@@ -3,17 +3,46 @@
 namespace controller;
 
 require_once("src/view/NewMixtapeView.php");
+require_once("src/view/MessageView.php");
 
 class NewMixtapeController {
+
+    private $view;
+    private $messages;
+
+    public function getPostedMixtapeName() {
+        return $_POST["mixtapeName"];;
+    }
+    public function getPostedMixtapeLinks() {
+        return $_POST["mixtapeLinks"];;
+    }
 
     // Constructor, connects all the layers
     public function __construct() {
         $this->view = new \view\NewMixtapeView();
+        $this->messages = new \view\MessageView();
     }
 
     public function checkActions() {
 
         // NOTE TO SELF, A URI HAZ 22 chars. Can prob use explode().
+
+        // If a user tries to login, the input is checked and validated.
+        if($this->view->onClickAddMixtape())
+        {
+            if ($this->getPostedMixtapeName() == "")
+            {
+                $this->messages->save("Mixtape name is missing");
+            }
+            elseif ($this->getPostedMixtapeLinks() == "")
+            {
+                $this->messages->save("No mixtape links added, mixtape can't be empty");
+            }
+            else
+            {
+
+            }
+        }
 
         return $this->view->showPage();
     }
--- src/view/NewMixtapeView.php
+++ src/view/NewMixtapeView.php
@@ -2,6 +2,26 @@
 namespace view;
 
 class NewMixtapeView {
+
+    private $messages;
+
+    public function __construct()
+    {
+        $this->messages = new \view\MessageView();
+    }
+
+    // Checks if the user has pressed the createMixtapeButton.
+    public function onClickAddMixtape() {
+        if(isset($_POST["createMixtapeButton"]))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
     public function showPage() {
         return "<div class='container'>
             <h1>Create a new mixtape</h1>
@@ -11,11 +31,11 @@ class NewMixtapeView {
 
              <form action='' method='post' name='newMixtapeForm'>
                 <fieldset>
-                <legend>Create your new mixtape</legend>
+                <legend>Create your new mixtape</legend><p>" . $this->messages->load() . "</p>
                 <label><strong>Mixtape name: </strong></label>
                 <input type='text' name='mixtapeName' class='form-control' value='' /><br />
                 <label><strong>Mixtape songs: </strong></label>
-                <textarea class='form-control' rows='20'></textarea><br />
+                <textarea class='form-control' rows='20' name='mixtapeLinks'></textarea><br />
                 <input type='submit' value='Create mixtape' name='createMixtapeButton' class='btn btn-default' />
                 </fieldset>
             </form>
