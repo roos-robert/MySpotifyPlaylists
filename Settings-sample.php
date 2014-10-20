8c5e0bddb63f4e67e892a1adc2e2afceee345c50
rr222cy <rr222cy@student.lnu.se>
2014-10-20 17:19:19 +0200

    Correction of Setting-sample.php

--- Settings-sample.php
+++ Settings-sample.php
@@ -1,9 +1,10 @@
+<?php
+
 /*
 This is a class for settings that apply to your whole application.
 Please enter the correct details, then save this file as "Settings.php" before uploading the project to your host.
 */
 
-<?php
 
 class Settings {
     public static $DB_USERNAME = "";
